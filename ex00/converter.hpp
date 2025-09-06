e894254b306a09f4d0117a89f236d8602895361f
Lepokile <yahya.elboukili1@gmail.com>
2025-09-06 16:20:00 -0400

    started CPP06

---
+++ ex00/converter.hpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   converter.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-bouk <yel-bouk@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/06 16:13:01 by yel-bouk          #+#    #+#             */
+/*   Updated: 2025/09/06 16:18:13 by yel-bouk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include <iostream>
+# include <iomanip>
+# include <exception>
+# include <cstdlib>
+# include <limits>
+# include <cmath>
+# include <string>
+
+# define MIN_INT std::numeric_limits<int>::min() //-2147483648
+# define MAX_INT std::numeric_limits<int>::max() //2147483647
+# define MIN_FLOAT std::numeric_limits<float>::min() //-3.40282e+38
+# define MAX_FLOAT std::numeric_limits<float>::max() //3.40282e+38
+# define MIN_DOUBLE std::numeric_limits<double>::min() //-1.79769e+308
+# define MAX_DOUBLE std::numeric_limits<double>::max() //1.79769e+308
+
+class ScalarConverter
+{
+public:
+	static void convert(const std::string &literal);
+private:
+	ScalarConverter();
+	ScalarConverter(const ScalarConverter &other);
+	ScalarConverter &operator=(const ScalarConverter &other);
+	~ScalarConverter();
+
+	static bool isChar(const std::string &literal);
+	static bool isInt(const std::string &literal);
+	static bool isFloat(const std::string &literal);
+	static bool isDouble(const std::string &literal);
+
+	static void convertFromChar(char c);
+	static void convertFromInt(int i);
+	static void convertFromFloat(float f);
+	static void convertFromDouble(double d);
+
+	static void printChar(double d);
+	static void printInt(double d);
+	static void printFloat(double d);
+	static void printDouble(double d);
+}
